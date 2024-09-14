35211ef9ffc83a644e047ce9bc2a3f018ba9bc61
Aliaksandr_Ravinski <sasha_ravinsky@tut.by>
2024-09-14 12:45:03 +0300

    Added new widget for lobby

--- Source/Parcheesi/Private/UI/Widgets/CCGameLobbyUI.cpp
+++ Source/Parcheesi/Private/UI/Widgets/CCGameLobbyUI.cpp
@@ -6,14 +6,24 @@
 
 void UCCGameLobbyUI::NativeConstruct()
 {
-    StartGameButton->OnClicked.AddDynamic(this, &UCCGameLobbyUI::StartGameButtonClicked);
-    SelectRedButton->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectRedButtonClicked);
-    SelectYellowButton->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectYellowButtonClicked);
-    SelectGreenButton->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectGreenButtonClicked);
-    SelectBlueButton->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectBlueButtonClicked);
+    B_StartGame->OnClicked.AddDynamic(this, &UCCGameLobbyUI::StartGameButtonClicked);
+    B_SelectRed->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectRedButtonClicked);
+    B_SelectYellow->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectYellowButtonClicked);
+    B_SelectGreen->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectGreenButtonClicked);
+    B_SelectBlue->OnClicked.AddDynamic(this, &UCCGameLobbyUI::SelectBlueButtonClicked);
 
     if (!GetOwningPlayer()->HasAuthority())
-        StartGameButton->SetIsEnabled(false);
+    {
+        B_StartGame->SetIsEnabled(false);
+        B_BotRed->SetIsEnabled(false);
+        B_BotRed->SetVisibility(ESlateVisibility::Hidden);
+        B_BotYellow->SetIsEnabled(false);
+        B_BotYellow->SetVisibility(ESlateVisibility::Hidden);
+        B_BotGreen->SetIsEnabled(false);
+        B_BotGreen->SetVisibility(ESlateVisibility::Hidden);
+        B_BotBlue->SetIsEnabled(false);
+        B_BotBlue->SetVisibility(ESlateVisibility::Hidden);
+    }
 }
 
 void UCCGameLobbyUI::StartGameButtonClicked()
--- Source/Parcheesi/Public/UI/Widgets/CCGameLobbyUI.h
+++ Source/Parcheesi/Public/UI/Widgets/CCGameLobbyUI.h
@@ -7,11 +7,12 @@
 #include "CCGameLobbyUI.generated.h"
 
 class UButton;
+class UTextBlock;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStartGameButtonPressed);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnColorButtonPressed, FName, ColorTag);
 
-    UCLASS() class PARCHEESI_API UCCGameLobbyUI : public UUserWidget
+UCLASS() class PARCHEESI_API UCCGameLobbyUI : public UUserWidget
 {
     GENERATED_BODY()
 
@@ -23,20 +24,67 @@ public:
     FOnColorButtonPressed OnColorButtonPressed;
 
 protected:
+    // Global lobby buttons
     UPROPERTY(meta = (BindWidget))
-    class UButton* StartGameButton;
+    class UButton* B_StartGame;
 
     UPROPERTY(meta = (BindWidget))
-    class UButton* SelectRedButton;
+    class UButton* B_ExitLobby;
 
     UPROPERTY(meta = (BindWidget))
-    class UButton* SelectYellowButton;
+    class UButton* B_SaveSettings;
+
+    // Pawn selecting buttons
 
     UPROPERTY(meta = (BindWidget))
-    class UButton* SelectGreenButton;
+    class UButton* B_SelectRed;
 
     UPROPERTY(meta = (BindWidget))
-    class UButton* SelectBlueButton;
+    class UButton* B_SelectYellow;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_SelectGreen;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_SelectBlue;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_BotRed;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_BotYellow;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_BotGreen;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* B_BotBlue;
+
+    // Pawn selecting text boxes
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_RedPlayerName;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_GreenPlayerName;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_YellowPlayerName;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_BluePlayerName;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_RedPlayerReady;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_GreenPlayerReady;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_YellowPlayerReady;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* T_BluePlayerReady;
 
     UFUNCTION()
     void StartGameButtonClicked();
